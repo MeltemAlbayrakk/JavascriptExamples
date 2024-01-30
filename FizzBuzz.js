7fdb1ed802512031d886a17481fcd57ade8b2e9b
MeltemAlbayrakk <77754573+MeltemAlbayrakk@users.noreply.github.com>
2024-01-30 11:06:30 +0300

    fix: FizzBuzz example

---
+++ FizzBuzz.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Lütfen bir değer girin: ', (input) => {
+  console.log('Girdiğiniz değer:', input);
+  if (input%3===0 && input%5!==0) {
+    console.log("Fizz")
+  }else if(input%5===0 && input%3!==0){
+    console.log("Buzz")
+  }else if(input%5===0 && input%3===0){
+    console.log("FizzBuzz")
+  }
+  rl.close();
+});
