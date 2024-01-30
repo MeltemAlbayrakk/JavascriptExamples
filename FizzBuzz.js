8336a2ad38599a7d3ce184cea1ff669de8474981
MeltemAlbayrakk <77754573+MeltemAlbayrakk@users.noreply.github.com>
2024-01-30 11:17:11 +0300

    fix: some bugs

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -9,7 +9,7 @@ rl.question('Lütfen bir değer girin: ', (input) => {
   console.log('Girdiğiniz değer:', input);
   if (input%3===0 && input%5!==0) {
     console.log("Fizz")
-  }else if(input%5===0 && input%3!==0){
+  }else if(input%5===0 ){
     console.log("Buzz")
   }else if(input%5===0 && input%3===0){
     console.log("FizzBuzz")
